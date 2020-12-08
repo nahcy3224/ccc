6b0e3803d06f4e068a9972eeed0d0b1ddae8f403
JFong Chen <you@example.com>
2020-12-09 06:43:16 +0800

    新增HomeContrller

---
+++ CHOY/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHOY.Controllers
+{
+  public class HomeController : Controller
+  {
+    // GET: Home
+    public ActionResult Index()
+    {
+      if (TempData["Error"] != null)
+        ViewBag.Error = TempData["Error"];
+
+      return View();
+    }
+  }
+}
